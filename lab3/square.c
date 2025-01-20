a3b6ef1d03212a9f18005f05dbf090246edc0b20
mmandernach13 <mmandernach13@gmail.com>
2025-01-20 23:40:37 +0000

    programs to find area of square or triangle from user input

---
+++ lab3/square.c
@@ -0,0 +1,28 @@
+/*
+ * Author: Mason Mandernach
+ * For: CSCE 155E
+ * On: 20/01/2025
+ * Description: caclulate area of square from user inputs
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv){
+
+    printf("Enter side length of square: ");
+    double side = 0;
+    scanf(&side);
+
+    if(side <= 0){
+        printf("Invalid side length");
+        exit(1);
+    }
+
+    double area = side * side;
+
+    printf("Area of square is: %.2lf", area);
+
+    return 0;
+}
---
+++ lab3/triangle.c
@@ -0,0 +1,36 @@
+/*
+ * Author: Mason Mandernach
+ * For: CSCE 155E
+ * On: 20/01/2025
+ * Description: calculate area of triangle from user input
+ * 
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv){
+
+    printf("Enter height of triangle: ");
+    double height = 0;
+    scanf(&height);
+
+    if(height <= 0){
+        printf("Invalid height");
+        exit(1);
+    }
+
+    printf("Enter width of triangle: ");
+    double width = 0;
+    scanf(&width);
+
+    if(width <= 0){
+        printf("Invalid width");
+        exit(1);
+    }
+
+    double area = 0.5*height*width;
+
+    printf("Area of square is: %.2lf", area);
+    
+    return 0;
+}
