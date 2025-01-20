970a7f4665961f6688f7200b305c66f95c7961b2
mmandernach13 <mmandernach13@gmail.com>
2025-01-20 23:09:45 +0000

    finished prompt

--- lab1/first.c
+++ lab1/first.c
@@ -10,5 +10,9 @@
 
 int main(int argc, char** argv){
 
-    printf("hello world\n");
+    printf("This is my first program\n");
+    printf("My name is Mason\n");
+    printf("My favorite ice cream is mint chip\n");
+
+    return 0;
 }
