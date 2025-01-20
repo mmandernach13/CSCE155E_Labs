5e0a271825141f6e8a080243d70d9d4116cb6bf1
mmandernach13 <mmandernach13@gmail.com>
2025-01-20 22:57:59 +0000

    made first program

---
+++ lab1/first.c
@@ -0,0 +1,14 @@
+/*
+ * Author: Mason Mandernach
+ * For: CSCE 155E
+ * On: 20/01/2025
+ * Description: simple starter program
+ * 
+ */
+
+#include <stdio.h>
+
+int main(int argc, char** argv){
+
+    printf("hello world\n");
+}
